3ee58b76a228544efdd729531c580c93be8abfdd
m-emadeddin <mohamed.emad12363@gmail.com>
2022-11-29 11:22:19 +0200

    Under Construction...

---
+++ task-2/Problem-1/cpp.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+using namespace std;
+
+/**
+ *  Write a ???? program that randomly generates a 3-digit number. 
+ *  Ask the user to guess a 3-digit number. For every digit that the user guessed correctly in the correct place,
+ *  they have a "hit". For every digit the user guessed correctly in the wrong place is a "miss".
+ *  Every time the user makes a guess, tell them how many hits and misses they have. 
+ *  Once the user guesses the correct number, the game is over. print the number of attempts they needed to guess the number.
+ */
+
+int rand(int a, int b){
+    return a + rand() & (b - a + 1);
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0),cout.tie(0);
+
+    srand((time(NULL)));
+    
+    int random = rand(100,999);
+    
+    char const* randomArray = to_string(random).c_str();
+
+    int cnt = 0;
+    int userGuess = 0;
+    cout << randomArray << endl;
+    cout << "Enter a 3-digit number: ";
+    cout.flush();
+    while(true){
+        cnt++;
+        bool vis[3]{};
+        cin >> userGuess;
+        while(userGuess>999 || userGuess<100){
+            cout << "Re-enter your input in the required format -a7snlk-, please: ";
+            cout.flush();
+            cin >> userGuess;
+        }
+        char const* userArray = to_string(userGuess).c_str();
+        int hits = 0, misses = 0;
+        for(int i=0; i<3; i++){
+            if(userArray[i] == randomArray[i]){
+                hits++; vis[i]=true;
+            }
+        }
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                if(userArray[i] == randomArray[j] && !vis[j]){
+                    misses++;
+                    vis[j]=true;
+                }
+            }
+        }
+        if(hits==3) break;
+        cout << "Your Guess Has: " << hits << (hits == 1 ? " hit ":" hits ") << "and " << misses << (misses == 1 ? " miss.":" misses.") << endl;
+        cout << "Try again. Please Enter a 3-digit number: ";
+        cout.flush();
+    }
+    cout << "Good Job! You guessed it correctly for only " << cnt << " Guesses." << endl;
+
+}
+
+
+
+
+
+
+
---
+++ task-2/Problem-1/py.py
@@ -0,0 +1,38 @@
+"""
+ *  Write a ???? program that randomly generates a 3-digit number. 
+ *  Ask the user to guess a 3-digit number. For every digit that the user guessed correctly in the correct place,
+ *  they have a "hit". For every digit the user guessed correctly in the wrong place is a "miss".
+ *  Every time the user makes a guess, tell them how many hits and misses they have. 
+ *  Once the user guesses the correct number, the game is over. print the number of attempts they needed to guess the number.
+"""
+import random
+
+rnd = random.randint(100,999)
+print(rnd)
+print("Enter a 3-digit number: ", end="")
+cnt = 0
+while True:
+    cnt += 1
+    vis = [False]*3
+    userGuess = int(input())
+    while(userGuess>999 or userGuess<100): 
+        print("Re-enter your input in the required format -a7snlk bs bl python-, please: ", end="")
+        userGuess = int(input())
+    hits = 0; misses = 0
+    for i in range(3):
+        if(str(userGuess)[i] == str(rnd)[i]):
+            hits +=1
+            vis[i] = True
+    
+    for i in range(3):
+        for j in range(3):
+            if(str(userGuess)[i] == str(rnd)[j] and not vis[j]):
+                misses += 1
+                vis[j] =  True
+    
+    if hits == 3 : break
+    strhit = " hit " if hits==1 else " hits "
+    strmiss = " miss " if hits==1 else " misses "
+    print(f"Your Guess Has: {hits} {strhit} and {misses} {strmiss}.")
+    print("Try again. Please Enter a 3-digit number: ")
+print(f"Good Job! You guessed it correctly for only {cnt} Guesses.")
